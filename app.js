c8b807ec4986776c380b1b4bb514b5065f3ead10
Abbas Siddiqi <abbas.siddiqi@yahoo.com>
2017-08-12 03:03:20 +0500

    Update port configuration in app.js

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ app.use('/', routes);
 // });
 
 // Server
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('listening on port 3000');
 });
